6776e3c9efa52170d1f45b3b85b057dfb531085f
isaiah <isaiahballah@gmail.com>
2019-01-27 11:49:27 -0500

    Backend Api updates

--- TruckSolution/bin/models/cargo.dart
+++ TruckSolution/bin/models/cargo.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-class Cargo{
+class Cargo {
 
   final int id;
   bool _moving;
@@ -9,7 +9,6 @@ class Cargo{
   Cargo(this.id, int moved, this.nextDep, this.status){
     _moving = moved>=1?true:false;
   }
-
 
   factory Cargo.fromJson(String json) {
     Map<String, dynamic> map;
@@ -45,5 +44,48 @@ class Cargo{
   bool getMoving(){
     return _moving;
   }
+}
 
+Cargo fakeCargo = new Cargo(1, 1, "00:00:00", "Delivering");
+Cargo stolenCargo = new Cargo(1, 1, "00:00:00", "Stolen");
+
+List<Cargo> fakeCargos = [
+  new Cargo(1, 1, "01:00:00", "Delivering"),
+  new Cargo(2, 1, "10:00:00", "Delivering"),
+  new Cargo(3, 1, "00:05:00", "Delivering"),
+  new Cargo(4, 1, "00:00:00", "Delivering"),
+  new Cargo(5, 1, "02:00:00", "Delivering"),
+  new Cargo(6, 1, "00:00:00", "Delivering"),
+  new Cargo(7, 1, "00:00:00", "Delivering"),
+  new Cargo(8, 1, "00:00:00", "Delivering"),
+  new Cargo(9, 1, "04:00:00", "Delivering"),
+  new Cargo(10, 1, "00:00:00", "Delivering"),
+  new Cargo(11, 1, "00:05:00", "Delivering"),
+  new Cargo(12, 1, "00:00:00", "Delivering"),
+  new Cargo(13, 1, "00:00:00", "Delivering"),
+  new Cargo(14, 1, "3:00:00", "Delivering"),
+  new Cargo(15, 1, "00:00:00", "Delivering"),
+  new Cargo(16, 1, "0:00:00", "Delivering"),
+  new Cargo(17, 1, "00:00:00", "Delivering")
+];
+
+
+String getFakeCargosJson() {
+  return getJsonListFromCargoList(fakeCargos);
+}
+
+
+List<Cargo> getCargoListFromJson(String json) {
+  List<Cargo> cargos = new List();
+  List<dynamic> dynamicList = jsonDecode(json);
+  for (String cargoJson in dynamicList)
+    cargos.add(Cargo.fromJson(cargoJson));
+  return cargos;
+}
+
+String getJsonListFromCargoList(List<Cargo> cargos) {
+  List<String> strings = new List();
+  for (Cargo cargo in cargos)
+    strings.add(cargo.toJson());
+  return jsonEncode(strings);
 }
