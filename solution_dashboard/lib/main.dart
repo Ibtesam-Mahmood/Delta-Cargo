2fc50126a7a0b47c9826aaafee663f3e0fcf657f
Ibtesam Mahmood <ibtesam.mahmood2@gmail.com>
2019-01-27 07:44:41 -0500

    listview finished

--- solution_dashboard/lib/main.dart
+++ solution_dashboard/lib/main.dart
@@ -12,9 +12,11 @@ class AppDashBoard extends StatelessWidget {
       title: "Solution",
       theme: ThemeData(
         primaryColor: Color(0xFF03254E),
+        textSelectionColor: Color(0xFF545677),
         accentColor: Color(0xFF545677)
       ),
       home: Scaffold(
+        backgroundColor: Color(0xFF3B4975),
         body: DashBoard(),
         appBar: AppBar(
           title: Text("Cargo Dashboard"),
@@ -35,12 +37,12 @@ class DashBoard extends StatefulWidget {
 }
 
 class _DashBoardState extends State<DashBoard> {
-  final List<Cargo> cargoList = [Cargo(1, 0, "00:00:00", "Delivered")];
+  final List<Cargo> cargoList = [Cargo(1, 1, "00:00:00", "Delivering"), Cargo(1, 0, "17:50:00", "Delivered"), Cargo(1, 1, "15:03:00", "Stolen")];
 
   Icon _getTrailingIcon(String status){
-    if(status=="Delivered") return Icon(Icons.card_giftcard, color: Colors.green,);
-    else if(status=="Delivering") return Icon(Icons.local_shipping, color: Colors.teal);
-    else return Icon(Icons.warning, color: Colors.red,);
+    if(status=="Delivered") return Icon(Icons.card_giftcard, color: Colors.teal,);
+    else if(status=="Delivering") return Icon(Icons.local_shipping, color: Colors.cyan);
+    else return Icon(Icons.warning, color: Color(0xFFCC0000),);
   }
 
         //   title: Text(cargoList[i].nextDep.toString()),
@@ -52,7 +54,7 @@ class _DashBoardState extends State<DashBoard> {
       elevation: 3,
       child: ListTile(
         leading: TitledWidget(text: "Status", widget: _getTrailingIcon(cargoList[i].status),),
-        title: Text("Next Departure:" + cargoList[i].nextDep.toString()),
+        title: Text("Next Departure:" + cargoList[i].nextDep.substring(0,5).toString()),
         subtitle: Text("ID: " + cargoList[i].id.toString()),
         trailing: TitledWidget(text: "Moving", widget: Icon(cargoList[i].getMoving()?Icons.forward:Icons.block)),
       ),
@@ -61,9 +63,12 @@ class _DashBoardState extends State<DashBoard> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: cargoList.length,
-      itemBuilder: (context, i) => _listTimeBuilder(context, i),
+    return Container(
+      margin: EdgeInsets.only(top: 5),
+      child: ListView.builder(
+        itemCount: cargoList.length,
+        itemBuilder: (context, i) => _listTimeBuilder(context, i),
+      ),
     );
   }
 }
