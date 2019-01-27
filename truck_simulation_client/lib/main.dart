719ec6d6fd95e8187d1fc9f3107638ef2c4c2bf2
Ibtesam Mahmood <ibtesam.mahmood2@gmail.com>
2019-01-27 11:23:00 -0500

    truck client finished

--- truck_simulation_client/lib/main.dart
+++ truck_simulation_client/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:numberpicker/numberpicker.dart';
+import 'dart:convert';
 
 void main() => runApp(ClientApp());
 
@@ -26,11 +29,73 @@ class ClientApp extends StatelessWidget {
   }
 }
 
-class LayoutWidget extends StatelessWidget {
+class LayoutWidget extends StatefulWidget {
+
+  @override
+  LayoutWidgetState createState() {
+    return new LayoutWidgetState();
+  }
+}
+
+class LayoutWidgetState extends State<LayoutWidget> {
+  int id = 1;
+  String timeToDeparture = "00:00";
+
+  Future<void> _setMove() async{
+    await http.get('http://isaiah.localhost.run/setMove?id=${id}&move=true');
+  }
+
+  Future<void> fetchPost() async {
+    //Parameters to the API call
+    final response =
+        await http.get("http://isaiah.localhost.run/getTruck");
+
+
+    if (response.statusCode == 200) {
+      // If server returns an OK response, parse the JSON
+      timeToDeparture = json.decode(response.body)['nextDep'];
+    } else {
+      // If that response was not OK, throw an error.
+      print('Failed to load post');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    fetchPost();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("ID", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 40)),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+              ),
+              child: NumberPicker.integer(
+                initialValue: 1,
+                minValue: 1,
+                maxValue: 17,
+                onChanged: (newValue) =>setState((){
+                  id = newValue;
+                  fetchPost();
+                })
+              ),
+            ),
+            RaisedButton(child: Text("Move Vehicle " + id.toString()), onPressed: (){_setMove();},),
+            Padding(padding: EdgeInsets.all(10),),
+            Text("Next Departure At: ${timeToDeparture.substring(0,5)}", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.red, fontSize: 20)),
+          ],
+        ),
+      ),
     );
   }
 }
